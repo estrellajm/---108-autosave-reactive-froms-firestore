ff506e9815906dd4c093c91a92772f6a6674ecb3
Jose Estrella <jose.estrella@imploie.com>
2018-06-02 13:43:25 -0600

    synced form with firestore

--- src/app/form-demo/form-demo.component.ts
+++ src/app/form-demo/form-demo.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms'
-import { AngularFirestore } from 'angularfire2/firestore'
-import { BehaviorSubject } from 'rxjs'
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'form-demo',
@@ -9,29 +9,39 @@ import { BehaviorSubject } from 'rxjs'
   styleUrls: ['./form-demo.component.scss']
 })
 export class FormDemoComponent implements OnInit {
-
   myForm: FormGroup;
   myDoc;
 
   state: string;
 
-  constructor(private fb: FormBuilder, private afs: AngularFirestore) { }
+  constructor(private fb: FormBuilder, private afs: AngularFirestore) {}
 
   ngOnInit() {
+    this.formInit();
+    this.formSync();
+  }
 
+  formInit() {
     this.myForm = this.fb.group({
       email: ['', Validators.required],
       career: ['', Validators.required],
       bio: ['']
-    })
-
-    this.myDoc = this.afs.doc('contacts/test').valueChanges();
+    });
   }
 
+  formSync() {
+    this.myDoc = this.afs.doc('contacts/test').valueChanges();
+    this.afs
+      .doc('contacts/test')
+      .valueChanges()
+      .subscribe(a => {
+        this.myForm.patchValue(a);
+        this.state = 'synced';
+      });
+  }
 
   changeHandler(e) {
     // console.log(e)
     this.state = e;
-
   }
 }
