525b10f636fda479480f1463e66a4395a50edb51
Jose Estrella <jose.estrella@imploie.com>
2018-06-02 17:22:41 -0600

    .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,20 +6,12 @@ export const environment = {
   production: false,
   firebase: {
     // Add your project credentials
-    // apiKey: '<your-key>',
-    // authDomain: '<your-project-authdomain>',
-    // databaseURL: '<your-database-URL>',
-    // projectId: '<your-project-id>',
-    // storageBucket: '<your-storage-bucket>',
-    // messagingSenderId: '<your-messaging-sender-id>'
-
-    
-    apiKey: 'AIzaSyBWRISJCMt9q7tgT1nkNHE4DZXdDWuoE9U',
-    authDomain: 'dev-env-1a3a6.firebaseapp.com',
-    databaseURL: 'https://dev-env-1a3a6.firebaseio.com',
-    projectId: 'dev-env-1a3a6',
-    storageBucket: 'dev-env-1a3a6.appspot.com',
-    messagingSenderId: '283816088223'
+    apiKey: '<your-key>',
+    authDomain: '<your-project-authdomain>',
+    databaseURL: '<your-database-URL>',
+    projectId: '<your-project-id>',
+    storageBucket: '<your-storage-bucket>',
+    messagingSenderId: '<your-messaging-sender-id>'
   }
 };
 
